e78c5f3230936ad08754f5d8848d09fb0acea6c4
Milena Tomova <kokolina1888@gmail.com>
2022-05-22 18:55:03 +0300

    up to 114

--- components/common/hero/index.js
+++ components/common/hero/index.js
@@ -5,8 +5,12 @@
 export default function Hero() {
 
   return (
-    <section className="lg:2/6 text-left my-28">
-      <div className="text-6xl font-semibold text-gray-900 leading-none">Grow your career as a developer</div>
+    <section 
+      className="lg:2/6 text-left my-28"
+      >
+      <div 
+        className="text-6xl font-semibold text-gray-900 leading-none">
+        Grow your career as a developer</div>
       <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Learn programming and web development the easy way! Get unlimited access to all of our courses.</div>
       <div className="mt-5 sm:mt-8 flex lg:justify-start">
         <div className="rounded-md shadow">
