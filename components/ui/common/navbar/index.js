980908ea77b6dd39f14193550f56fc90c7078206
Milena Tomova <kokolina1888@gmail.com>
2022-05-23 20:56:07 +0300

    #130

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,20 +1,17 @@
 import Link from 'next/link'
 import { useWeb3 } from '@components/providers/web3' 
 import { Button } from '@components/ui/common';
-import { useRouter } from 'next/router';  
 import { useAccount } from '@components/web3/hooks/useAccount';
 
 
 export default function Footer() {
   const { connect, isLoading, isWeb3Loaded, web3, hooks  } = useWeb3();
-  const router = useRouter(); 
   const { account } = useAccount();
-  console.log( account )
   
   return (
 
-    <section> 
-     
+    <section>  
+    {account}
       <div className="relative px-4 pt-6 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex items-center justify-between">
@@ -48,8 +45,8 @@ export default function Footer() {
               <Link
                 href="/" 
                 >
-                <a 
-                 
+                <a  
+
                   className="mr-8 font-medium text-gray-500 hover:text-gray-900">
                   Wishlist
                 </a>
@@ -62,13 +59,20 @@ export default function Footer() {
                   Loading...
                 </Button> :  
                   isWeb3Loaded ?
+                    account ?
+                    <Button  
+                      className='cursor-default'
+                      hoverable={false}
+                    >
+                      Hi, there
+                    </Button>  :
+                    <Button 
+                      onClick={connect}
+                    >
+                      Connect
+                    </Button> :
                   <Button
-                    onClick={connect}
-                  >
-                    Connect 
-                  </Button>  :
-                  <Button
-                  onClick={()=>router.push('https://metamask.io/download/')}
+                  onClick={()=>window.open('https://metamask.io/download/')}
                 >
                   Install Metamask
                 </Button>  
