f012270a1e50cefc2c80a29dca77f17e1e4a0eb4
Milena Tomova <kokolina1888@gmail.com>
2022-05-23 22:41:31 +0300

    #136

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,10 +2,12 @@ import { useWeb3 } from "@components/providers"
 import Link from "next/link"
 import { Button } from "@components/ui/common"
 import { useAccount } from "@components/hooks/web3/useAccount"
+import { useRouter } from "next/router"
 
 export default function Footer() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3()
-  const { account } = useAccount()
+  const { account } = useAccount();
+  const { pathname } = useRouter()
 
   return (
     <section>
@@ -65,10 +67,11 @@ export default function Footer() {
           </div>
         </nav>
       </div>
-      { account.data &&
+      { account.data && 
+        !pathname.includes('/marketplace') &&
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="p-2 text-white bg-indigo-600 rounded-md">
-            {account.data}
+            { account.data }
           </div>
         </div>
       }
