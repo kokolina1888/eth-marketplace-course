cff6df352f5367d7709e2545f6c484c75cff4c27
Milena Tomova <kokolina1888@gmail.com>
2022-05-23 22:29:42 +0300

    #135

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,22 +1,33 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import useSWR from "swr"
 
+const adminAddresses = { 
+    '0xcd9b83001240ee15d072490a52030d1a0f24f06efa0f14788ce5a5f1c95c5b1a': true
+};
 export const handler = (web3, provider) => () => {
-  const [account, setAccount] = useState(null)
 
-  useEffect(() => {
-    const getAccount = async () => {
-      const accounts = await web3.eth.getAccounts()
-      setAccount(accounts[0])
+  const { data, mutate, ...rest } = useSWR(() =>
+    web3 ? "web3/accounts" : null,
+    async () => {
+      const accounts = await web3.eth.getAccounts();
+      return accounts[0];
     }
-
-    web3 && getAccount()
-  }, [web3])
+  )
 
   useEffect(() => {
-      provider && 
-      provider.on('accountsChanged', 
-        accounts => setAccount( accounts[0] ?? null ) )
+    provider &&
+    provider.on("accountsChanged",
+      accounts => mutate(accounts[0] ?? null)
+    )
   }, [provider])
-
-  return { account }
+if(data ){
+    console.log(web3.utils.keccak256(data) )
+}
+    return { account: {
+        isAdmin:  ( data && adminAddresses[web3.utils.keccak256(data)]) ?? false,  
+        data, 
+        mutate, 
+        ...rest
+        }
+    }
 }
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,87 +1,79 @@
-import Link from 'next/link'
-import { useWeb3 } from '@components/providers/web3' 
-import { Button } from '@components/ui/common';
-import { useAccount } from '@components/web3/hooks/useAccount';
-
+import { useWeb3 } from "@components/providers"
+import Link from "next/link"
+import { Button } from "@components/ui/common"
+import { useAccount } from "@components/web3/hooks/useAccount"
 
 export default function Footer() {
-  const { connect, isLoading, isWeb3Loaded, web3, hooks  } = useWeb3();
-  const { account } = useAccount();
-  
-  return (
+  const { connect, isLoading, isWeb3Loaded } = useWeb3()
+  const { account } = useAccount()
 
-    <section>  
-    {account}
+  return (
+    <section>
       <div className="relative px-4 pt-6 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex items-center justify-between">
             <div>
-              <Link
-                href="/" 
-              >
-                <a  
+              <Link href="/" >
+                <a
                   className="mr-8 font-medium text-gray-500 hover:text-gray-900">
                   Home
                 </a>
               </Link>
-              <Link
-                href="/" 
-              >
-                <a  
+              <Link href="/" >
+                <a
                   className="mr-8 font-medium text-gray-500 hover:text-gray-900">
                   Marketplace
                 </a>
               </Link>
-              <Link
-                href="/" 
-              >
-                <a  
+              <Link href="/" >
+                <a
                   className="mr-8 font-medium text-gray-500 hover:text-gray-900">
                   Blogs
                 </a>
-              </Link> 
+              </Link>
             </div>
-            <div> 
-              <Link
-                href="/" 
-                >
-                <a  
-
+            <div>
+              <Link href="/" >
+                <a
                   className="mr-8 font-medium text-gray-500 hover:text-gray-900">
                   Wishlist
                 </a>
               </Link>
-              { isLoading ? 
+              { isLoading ?
                 <Button
                   disabled={true}
-                  onClick={connect} 
-                >
-                  Loading...
-                </Button> :  
-                  isWeb3Loaded ?
-                    account ?
-                    <Button  
-                      className='cursor-default'
-                      hoverable={false}
-                    >
-                      Hi, there
-                    </Button>  :
-                    <Button 
-                      onClick={connect}
-                    >
-                      Connect
-                    </Button> :
-                  <Button
-                  onClick={()=>window.open('https://metamask.io/download/')}
-                >
+                  onClick={connect}>
+                    Loading...
+                </Button> :
+                isWeb3Loaded ?
+                account.data ?
+                <Button
+                  hoverable={false}
+                  className="cursor-default">
+                  Hi there { account.isAdmin && 'Admin' }
+                  {console.log(account.isAdmin)}
+                </Button> :
+                <Button
+                  onClick={connect}>
+                  Connect
+                </Button> :
+                <Button
+                  onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
                   Install Metamask
-                </Button>  
+                </Button>
               }
-             
             </div>
           </div>
         </nav>
       </div>
+      { account.data &&
+        <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+          <div className="p-2 text-white bg-indigo-600 rounded-md">
+            {account.data}
+          </div>
+        </div>
+      }
     </section>
   )
 }
+
