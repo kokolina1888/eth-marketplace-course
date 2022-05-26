ffb565354de7fb313204c489e5e641976ba734e0
Milena Tomova <kokolina1888@gmail.com>
2022-05-26 22:13:19 +0300

    #149

--- components/ui/common/button/index.js
+++ components/ui/common/button/index.js
@@ -19,4 +19,4 @@ export default function Button({
           {children}
       </button>
     )
-  }
+  } 
--- components/ui/course/courseCard/index.js
+++ components/ui/course/courseCard/index.js
@@ -8,17 +8,17 @@ export default function CourseCard({course, Footer }) {
     
     <div className="overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
         <div className="flex h-full">
-        <div className="flex h-full">
+        <div className="flex-1 h-full next-image-wrapper">
             <Image
             className="object-cover"
-            layout='fixed'
+            layout='responsive'
             width='200'
             height='230'
             src={course.coverImage}
             alt={course.title} 
             />
         </div>
-        <div className="p-8">
+        <div className="p-8 pb-4 flex-2">
             <div 
             className="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
             {course.type}
@@ -27,14 +27,14 @@ export default function CourseCard({course, Footer }) {
             href= {`/courses/${course.slug}`}
             >
             <a 
-                className="block mt-1 text-lg font-medium leading-tight text-black hover:underline">
+                className="block h-12 mt-1 text-lg font-medium leading-tight text-black hover:underline">
                 {course.title} 
             </a>
             </Link>
             
             <p 
             className="mt-2 text-gray-500">
-                {course.description}
+                {course.description.substring(0, 70)}...
             </p>
             {
                 Footer && 
