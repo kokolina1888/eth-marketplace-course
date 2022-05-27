544548fd43ca504ae7b6a7f2fa35d507b6a71dd1
Milena Tomova <kokolina1888@gmail.com>
2022-05-27 10:00:58 +0300

    #152

--- components/ui/order/modal/index.js
+++ components/ui/order/modal/index.js
@@ -1,38 +1,100 @@
-import { Modal } from "@components/ui/common"
+import { Modal, Button } from "@components/ui/common";
+import { useEffect, useState } from "react";
 
-export default function OrderModal() {
-    return (
-        <Modal isOpen={true}>
-        <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+
+export default function OrderModal({course, onClose}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if (!!course) {
+      setIsOpen(true)
+    }
+  }, [course])
+
+  const closeModal = () => {
+    setIsOpen(false)
+    onClose()
+  }
+
+  return (
+    <Modal isOpen={isOpen}>
+      <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
         <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
-            <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
-              <svg className="w-6 h-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-              </svg>
-            </div>
             <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">
-                Deactivate account
+              <h3 className="text-lg font-bold leading-6 text-gray-900 mb-7" id="modal-title">
+                {course.title}
               </h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+              <div className="relative mt-1 rounded-md">
+                <div className="mb-1">
+                  <label className="mb-2 font-bold">Price(eth)</label>
+                  <div className="flex text-xs text-gray-700">
+                    <label className="flex items-center mr-2">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox"
+                      />
+                    </label>
+                    <span>Adjust Price - only when the price is not correct</span>
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  className="block p-4 mb-1 border-gray-300 rounded-md shadow-md disabled:opacity-50 w-80 focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                />
+                <p className="text-xs text-gray-700">
+                  Price will be verified at the time of the order. If the price will be lower, order can be declined (+- 2% slipage is allowed)
                 </p>
+              </div>
+              <div className="relative mt-2 rounded-md">
+                <div className="mb-1">
+                  <label className="mb-2 font-bold">Email</label>
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="block p-4 border-gray-300 rounded-md shadow-md w-80 focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                  placeholder="x@y.com"
+                />
+                <p className="mt-1 text-xs text-gray-700">
+                It&apos;s important to fill a correct email, otherwise the order cannot be verified. We are not storing your email anywhere
+                </p>
+              </div>
+              <div className="relative my-2 rounded-md">
+                <div className="mb-1">
+                  <label className="mb-2 font-bold">Repeat Email</label>
+                </div>
+                <input
+                  type="email"
+                  name="confirmationEmail"
+                  id="confirmationEmail"
+                  className="block p-4 border-gray-300 rounded-md shadow-md w-80 focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm" placeholder="x@y.com" />
+              </div>
+              <div className="flex text-xs text-gray-700">
+                <label className="flex items-center mr-2">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox" />
+                </label>
+                <span>I accept Eincode &apos;terms of service&apos; and I agree that my order can be rejected in the case data provided above are not correct</span>
               </div>
             </div>
           </div>
         </div>
-        <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
-          <button type="button" className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-            Deactivate
-          </button>
-          <button type="button" className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+        <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex">
+          <Button>
+            Submit
+          </Button>
+          <Button
+            onClick={closeModal}
+            variant="red">
             Cancel
-          </button>
+          </Button>
         </div>
       </div>
-        </Modal>
-        
-    );
+    </Modal>
+  )
 }
