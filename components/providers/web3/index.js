78bef0ce649162f5cc63230d4acbda83ba45f37f
Milena Tomova <kokolina1888@gmail.com>
2022-05-31 20:12:04 +0300

    #182

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -1,5 +1,6 @@
 const { createContext, useContext, useState, useEffect, useMemo } = require("react");
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "@utils/loadContract";
 import Web3 from "web3"; 
 import { setupHooks } from "./hooks/setupHooks";
 
@@ -19,10 +20,11 @@ export default function Web3Provider({children}) {
             const provider = await detectEthereumProvider();
             if( provider) {
               const web3 = new Web3(provider);
+              const contract = loadContract('CourseMarketplace', provider)
               setWeb3Api({
                   provider,
                   web3,
-                  contract: null,
+                  contract,
                   hasInitialResponse: false,
                   hooks: setupHooks(web3, provider)
               })
