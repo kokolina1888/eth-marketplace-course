f5f056290286da3ff3becb0f36435dd30dc571a0
Milena Tomova <kokolina1888@gmail.com>
2022-06-01 17:42:28 +0300

    #187

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -9,24 +9,34 @@ import { MarketHeader } from "@components/ui/marketplace"
 import { useWeb3 } from "@components/providers"
  
 export default function Marketplace({courses}) {
-  const { web3 } = useWeb3() 
+  const { web3, contract } = useWeb3() 
   const [ selectedCourse, setSelectedCourse ] = useState(null)
   const { canPurchaseCourse, account } = useWalletInfo();
 
-  const purchaseCourse = order => {
+  const purchaseCourse = async ( order ) => {
     const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id) 
     const orderHash = web3.utils.soliditySha3(
       { type: 'bytes16', value: hexCourseId },
       { type: 'address', value: account.data }
     ) 
 
-    const emailHash = web3.utils.sha3(order.email) 
+    const emailHash = web3.utils.sha3(order.email)  
 
     const proof = web3.utils.soliditySha3(
       { type: 'bytes32', value: emailHash },
       { type: 'bytes32', value: orderHash },
     )
-    console.log(proof)
+    const value = web3.utils.toWei(String(order.price))
+
+    try {
+      const result = await contract.methods.purchaseCourse(
+        hexCourseId,
+        proof
+      ).send({from: account.data, })
+      console.log(result)
+    } catch {
+      console.log('purchase course: operation failed')
+    }
   }
   
   return (
