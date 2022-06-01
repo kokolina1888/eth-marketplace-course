02f7d1b0f06fed73fe9f1c2be05ff8dfc9374bf9
Milena Tomova <kokolina1888@gmail.com>
2022-06-01 15:36:45 +0300

    #186

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -3,15 +3,30 @@ import { CourseCard, CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher" 
 import { useWalletInfo } from "@components/hooks/web3"
-import { Button } from "@components/ui/common";
+import { Button } from "@components/ui/common"
 import { OrderModal } from "@components/ui/order"
 import { MarketHeader } from "@components/ui/marketplace"
+import { useWeb3 } from "@components/providers"
  
 export default function Marketplace({courses}) {
-  const [selectedCourse, setSelectedCourse] = useState(null)
-  const {  canPurchaseCourse } = useWalletInfo();
-  const purchaseCourse = ( order ) => {
-    alert(JSON.stringify(order))
+  const { web3 } = useWeb3() 
+  const [ selectedCourse, setSelectedCourse ] = useState(null)
+  const { canPurchaseCourse, account } = useWalletInfo();
+
+  const purchaseCourse = order => {
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id) 
+    const orderHash = web3.utils.soliditySha3(
+      { type: 'bytes16', value: hexCourseId },
+      { type: 'address', value: account.data }
+    ) 
+
+    const emailHash = web3.utils.sha3(order.email) 
+
+    const proof = web3.utils.soliditySha3(
+      { type: 'bytes32', value: emailHash },
+      { type: 'bytes32', value: orderHash },
+    )
+    console.log(proof)
   }
   
   return (
