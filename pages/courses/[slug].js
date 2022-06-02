2db119b84c322e425e3eb4a993f07932c5c6be77
Milena Tomova <kokolina1888@gmail.com>
2022-06-02 16:22:55 +0300

    #201

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -8,15 +8,17 @@ import { getAllCourses } from "../../content/courses/fetcher";
 import { BaseLayout } from "@components/ui/layout";
 import { useOwnedCourse } from "@components/hooks/web3";
 import { useAccount } from "@components/hooks/web3";
+import { Message} from '@components/ui/common';
 
 export default function Course({course}) {
   const { account } = useAccount()
   const { ownedCourse } = useOwnedCourse(course, account.data)
-console.log(ownedCourse)
+  const courseState = ownedCourse.data?.state; 
   return (
     <>
       <div className="py-4">
         <CourseHero 
+            hasOwner={ !!ownedCourse.data }
             title={course.title}
             description={course.description}
             image={course.coverImage}
@@ -24,6 +26,33 @@ console.log(ownedCourse)
       </div>
       <Keypoints 
         points={course.wsl} />
+      { course.state &&
+        <div className="max-w-5xl mx-auto">
+      {
+        courseState === 'Purchased' && 
+       
+          <Message>
+            Course is purchased and waiting for activation. Process can take up to 24 houts.
+          </Message>
+        
+      }
+      {
+        courseState === 'Activated' && 
+       
+          <Message>
+            Course is activated
+          </Message>
+        
+      }
+      {
+        courseState === 'Deactivated' && 
+       
+          <Message>
+            Course is deactivated
+          </Message>
+        
+      }
+      </div> }
       <Curriculum 
         locked={true}
       />
